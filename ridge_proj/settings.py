c52c6ce4e03911cdd240ea94428abc57e4747ea3
klaus-ndivo <charlesndivo847@gmail.com>
2025-05-27 13:53:36 +0300

    Add

--- ridge_proj/settings.py
+++ ridge_proj/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-9l7t^esu39p49=3j0w!=-s*k^r4(i2j2(f&n$(zuldc(oah25f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['talaridge-production.up.railway.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
